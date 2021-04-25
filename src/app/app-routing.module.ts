e85055bb3eed95ef3bf89770f5273a877c8b36ce
elkin Garzon <elkingarzonyaya@gmail.com>
2021-04-25 10:49:02 -0500

    ajustes iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: () => import('./panel/panel.module').then(m => m.PanelModule)
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/panel/panel.module.ts
+++ src/app/panel/panel.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PanelRoutingModule } from './panel-routing.module';
-
+import { IndexComponent } from './index.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PanelRoutingModule
-  ]
+	declarations: [
+		IndexComponent
+	],
+	imports: [
+		CommonModule,
+		PanelRoutingModule
+	]
 })
 export class PanelModule { }
